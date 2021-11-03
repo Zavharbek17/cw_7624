b6d8790b18251ce9db0e4115515f622da0ef7eb5
Zavharbek17 <zavharbek17@mail.ru>
2021-11-03 17:03:30 +0500

    DbContext folder made

---
+++ Cw1_Cloud_7624/DbContexts/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw1_Cloud_7624.Model;
+
+namespace Cw1_Cloud_7624.DbContexts
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
